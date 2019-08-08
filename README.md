06873c6fbaa811267637d496ea640764f0c74191
Jeremiah Garza <jag_tt@yahoo.com>
2019-08-08 12:29:28 -0500

    changed/fixed some game story text, starting to update README

--- README.md
+++ README.md
@@ -5,4 +5,11 @@
 
 ###### The user will click through the text adventure game, reading the story and choosing between choices given.
 
+![Opening game screen]
+(https://i.imgur.com/K7OvObg.png)
 
+![Game screen]
+(https://i.imgur.com/AeppUSm.png)
+
+![You Died In-game]
+(https://i.imgur.com/ObLtryr.png)
--- js/main.js
+++ js/main.js
@@ -1,53 +1,48 @@
 /*----- constants -----*/
-    $("#btn1").click(function () {
-        $("p").fadeIn(3000);
-       
-    });
-
 const storyline = {
     begin: '',
 
-    stroll: 'It was a beautiful night, cool and brisk, the light of the street lanterns cascading across the cobblestone street as Ada and Henry walked through the park. Ada gripped tighter to Henry\'s arm, he was a good one, quick to anger sometimes sure- but under that rough exterior was a kind man; she knew it. Lightning flashed and cracked across the sky, some distance away. "Should we head home?" Henry muttered',
+    stroll: 'It was a beautiful night, cool and brisk, the light of the street lanterns cascading across the cobblestone street as Ada and Henry walked through the park. Ada gripped tighter to Henry\'s arm, a newly married couple, every day together made the next one even brighter. Lightning flashed and cracked across the sky, some distance away. "Should we head home?" Henry asked',
 
     bushNoises: 'As you turn around and start making your way back home, the green and overgrown bushes on your right begin to shake violently...',
 
-    husbandDeath1: 'You continue to walk... but you begin to notice the silence... that\'s strange, not a soul in sight. Suddenly a thundering and a flash. Where is Henry?? You glance down and see only his...legs? Blood spouts from his lower torso as Henry\'s bottom half collapsed in a heap in front of you',
+    husbandDeath1: 'You continue to walk... but you begin to notice the silence... that\'s strange, not a soul in sight. Suddenly a thundering and a flash. Where is Henry?? You glance down and see only his...legs? Blood spouts from his lower torso as Henry\'s bottom half collapses in a heap in front of you',
 
-    grabHenry: 'you let out a blood curdling scream as you grab and clutch at Henry\'s remaining half, as the monster rounds back and rips your body to shreds as you stand there in utter shock',
+    grabHenry: 'You let out a blood curdling scream as you grab and clutch at Henry\'s remaining half, as the monster rounds back and rips your body to shreds as you stand there in utter shock',
 
     ripHenryRun: 'In shock, you scream "RUN HENRY!!!" at what is left of Henry\'s lower half, you attempt to run, but the creature easily outpaces you in your current state and rips you to shreds.',
 
-    investigateBush: 'you slowly approach the bush- in your experience, "curiosity killed the cat" is a myth... Just as you finish the thought, a man bursts from the bushes screaming',
+    investigateBush: 'You slowly approach the bush- in your experience, "curiosity killed the cat" is a myth... Just as you finish the thought, a man bursts from the bushes screaming',
 
-    run: 'you run away, nothing good has ever come from bushes and noises',
+    run: 'You run away, nothing good has ever come from bushes and noises',
 
-    chasingYou: 'you are running, but before long, you realize that something, some creature is now on the path behind you and is now galloping after you on all fours, gaining fast.',
+    chasingYou: 'You are running, but before long, you realize that something, some creature is now on the path behind you and is now galloping after you on all fours, gaining fast.',
 
     strangerDeath: 'Are you ok??! You choke out in utter shock, your eyes start to take in his current condition: he appears to have gashes and long cuts all over his body. Before he can answer further, there\'s a loud noise- and there protruding from his chest is a clawed hand',
 
-    fight: 'the dying stranger\'s eyes roll back in his head, as you grab this "things" hand yelling "You won\'t get away with this you fiend!!!!!" The creature grabs your shirt, and the top of your head with it\'s other hand, and pop!! You die from a mild head tear.',
+    fight: 'The dying stranger\'s eyes roll back in his head, as you grab this "things" hand yelling "You won\'t get away with this you fiend!!!!!" The creature grabs your shirt, and the top of your head with it\'s other hand, and pop!! You die from a mild head tear.',
 
     turnAndFight: 'You face your fears, and decide to turn around and fight whatever this thing is. As you do this, the creature runs through you from such speed and velocity, you explode into hundreds of pieces.',
 
-    run2: 'running for dear life, you trip on a loose stone, smashing your face hard against the pavement, the world goes dark just for a moment, but your gaze falls upon what appears to be a large stick just off the path.',  
+    run2: 'Running for dear life, you trip on a loose stone, smashing your face hard against the pavement, the world goes dark just for a moment, but your gaze falls upon what appears to be a large stick just off the path.',  
     
-    noticeStick: 'you notice a large stick to the side of the path, it\'s big but not too big to wield.',
+    noticeStick: 'You notice a large stick to the side of the path, it\'s big but not too big to wield.',
 
-    stick: 'you trip on your feet little, but grab the stick, you blindly swing back at the whirlwind of noise that is fast approaching you.  You are greeted with a sickening thud.',
+    stick: 'You trip on your feet little, but grab the stick, you blindly swing back at the whirlwind of noise that is fast approaching you.  You are greeted with a sickening thud.',
 
-    run3: 'there\'s no time to spare, you get your feet under you as best you can, and start running away... but you are far too slow now, defenseless and realize this too late as two strong hands rip your head from your body',
+    run3: 'There\'s no time to spare, you get your feet under you as best you can, and start running away... but you are far too slow now, defenseless and realize this too late as two strong hands rip your head from your body',
 
-    stickAgain: 'you swing down upon the creature again, another sickening thud but this time accompanied by a loud cry from this...thing. Warm blood splashes your arms and face.',
+    stickAgain: 'You swing down upon the creature again, another sickening thud but this time accompanied by a loud cry from this...thing. Warm blood splashes your arms and face.',
 
-    stickAgain2: 'you bring the stick down- again and again, the creature starts to will itself up as you begin to tire- someone shouts behind you "ahoy there! ye\'kay mate??!?"',
+    stickAgain2: 'You bring the stick down- again and again, the creature starts to will itself up as you begin to tire- someone shouts behind you "ahoy there! ye\'kay mate??!?"',
 
     youGotThis: 'I got this!! You yell with a big grin as you playfully brandish your stick at the downed creature, attempting to make your best Captain Morgan over the body, you fail to notice the monster whipping around and tearing off both your arms from their sockets. You bleed out and die.',
 
     callForHelp: '"Somebody, please help!!" you gutter as best you can, you look all around you, but the night is still and empty other than the creature and yourself. Meanwhile you took too long, and the creature got up and grabs you, using your head as a starter dish.',
 
-    helpArrives: 'the old man can see the terror in your eyes and comes running, the glint of a revolver catches your eye just as the creature yanks you down- a muffled flash and \'Blam! Blam! Blam!\'...the bloody hand around your arm loosens and falls.',
+    helpArrives: 'The old man can see the terror in your eyes and comes running, the glint of a revolver catches your eye just as the creature yanks you down- a muffled flash and \'Blam! Blam! Blam!\'...the bloody hand around your arm loosens and falls.',
 
-    fin: 'Thank you so much! you gasp, "yer alright now" the man retorts as he helps you to your feet. "What was that thing?" as you both turn to where the dead creature lays. The body is gone, glancing around the creature is nowhere to be found, and the air is still.'
+    fin: 'Thank you so much! you gasp, "HOLY HELL MATE!!" the man gasps as he helps you to your feet. "What was that thing?" as you both turn to where the dead creature lays. The body is gone, glancing around the creature is nowhere to be found, and the air is still.'
 };
 
 const pickedChoice = {
@@ -69,22 +64,20 @@ const pickedChoice = {
     callForHelp: {'btn1': '', 'btn2': ''},
     helpArrives: {'btn1': 'analyze the beast', 'btn2': 'thank the man'},
     youDied: {'btn1': 'YOU WERE TASTY', 'btn2': 'MEAT FOR THE CREATURE'},
-    fin: {'btn1': 'to be continued...', 'btn2': '...'},
+    fin: {'btn1': 'YOU SURVIVED!!!', 'btn2': ''},
 };
+
 
 
 
 /*----- app's state (variables) -----*/
 let story;
-// let i = 0;
-// let txt = story;
-// let speed = 50;
+
 
 /*----- cached element references -----*/
 const msgElem = document.getElementById('game-msg');
 const buttonOne = document.getElementById('btn1');
 const buttonTwo = document.getElementById('btn2');
-const cursorBlink = document.querySelector('span');
 
 
 
@@ -108,23 +101,19 @@ function render() {
     storySwitch()
 }
 
+$(document).ready(function() {
+$("#btn1").click(function () {
+    $("game-txt").fadeIn(3000);
+    }); 
+});
+
 function gameBegin(evt) {
     if (story === storyline.begin) {
         story = storyline.stroll;
-
     };
     render();
 }
 
-
-
-// function typeTxt() {
-//     if (i < txt.length) {
-//         document.getElementById('game-msg').innerHTML += txt.charAt(i); 
-//         i++;
-//         setTimeout(typeTxt, speed);
-//     }
-// }
 /*-----------------------------------------*/
 
 
@@ -327,4 +316,6 @@ function storySwitch() {
           default:  
     }
 }
+
+
 
